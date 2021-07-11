dc3bdadd99932ec53b68e7c0d581a3bf6507278c
mohammadraddad <d.dodo.dode@hotmail.com>
2021-07-11 13:54:45 +0300

    ninja

---
+++ OOP/ninja.js
@@ -0,0 +1,47 @@
+class Ninja {
+    
+    constructor(name, health,speed,strength ) {
+        
+        this.name = name;
+        this.health = health;
+        if(speed==null || strength==null){
+            this.strength=3;
+        this.speed=3;
+        }else{
+        this.speed=speed;
+        this.strength =strength;
+        }
+       
+    }
+
+
+    saymyname(){
+     console.log(this.name);
+   }
+
+   showStat(){
+       console.log(this.strength);
+       console.log(this.speed);
+       console.log(this.health);
+   }
+
+   drinkSake(){
+     this.health+=10;
+   }
+
+
+}
+
+
+
+    const n = new Ninja("mohammad",70,50,60);
+    n.saymyname();
+    
+    n.showStat();
+    
+    n.drinkSake();
+  
+    n.showStat();
+
+
+
