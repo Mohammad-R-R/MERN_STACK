94e7a0122b2ce9eb7d1caec20d68a779cc633795
mohammadraddad <d.dodo.dode@hotmail.com>
2021-07-12 21:08:06 +0300

    first jsx

---
+++ React/myfirst/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+
+    
+    <div className="App">
+     <h1>Hello dojo</h1>
+
+     <h2>things i need to do:</h2>
+     <ul>
+       <li>Learn React</li>
+       <li>climp Mt. Everest</li>
+       <li> Run a marathon
+       </li>
+       <li>feed the dogs</li>
+     </ul>
+    </div>
+  );
+}
+
+export default App;
