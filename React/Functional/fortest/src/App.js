a06fc852f16b6dff3092b3deb471b9148c826b10
mohammadraddad <d.dodo.dode@hotmail.com>
2021-07-24 23:06:04 +0300

    pokemon

--- React/Functional/fortest/src/App.js
+++ React/Functional/fortest/src/App.js
@@ -1,21 +1,16 @@
 import React, { useState } from 'react';
-import MessageForm from './components/form';
-import MessageDisplay from './components/twof';
-import MyComponent from './components/comp';
+
+import Example from './components/comp';
     
 
     
     
 function App() {
-  const [currentMsg, setCurrentMsg] = useState("There are no messages");
   
-  const youveGotMail = ( newMessage ) => {
-      setCurrentMsg( newMessage );
-  }
   
   return (
       <>
-        <MyComponent/>
+        <Example/>
          
       </>
   );
