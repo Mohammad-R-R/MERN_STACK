2c70079480428abf710458b53a7c6707e8d30509
mohammadraddad <d.dodo.dode@hotmail.com>
2021-07-25 11:34:19 +0300

    update

--- React/Apis/pokemonapi/src/components/coin.js
+++ React/Apis/pokemonapi/src/components/coin.js
@@ -2,18 +2,20 @@ import React,{useState,useEffect} from "react";
 
 const Example = (props) => {
     const [pokemon, setPeople] = useState([]);
- 
-    useEffect(() => {
+ const rem = (e) => {   
+   
         fetch('https://pokeapi.co/api/v2/pokemon?limit=807')
             .then(response => response.json())
-            .then((jsonPokemon) => setPeople(jsonPokemon.results))
-    }, []);
+            .then((x) => setPeople(x.results))
+           
+ };
  
     return (
         
         <div>
            
-                <button onClick={ useEffect}> get pokemons</button>
+                <button onClick={ rem}> get pokemons</button>
+              
                 {
                 pokemon.map((pokemon, index) => (
                     <div key={ index }>
@@ -21,8 +23,7 @@ const Example = (props) => {
                     </div>
                 ) )
             }
-            
         </div>
     );
 };
-export default Example
+export default Example;
